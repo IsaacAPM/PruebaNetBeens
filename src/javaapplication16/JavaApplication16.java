ffb2cb120bafbf71243c3751b64156813aacb544
unknown <isaacapimentelm@gmail.com>
2019-02-15 13:02:31 -0600

    Prueba 4

--- src/javaapplication16/JavaApplication16.java
+++ src/javaapplication16/JavaApplication16.java
@@ -17,7 +17,7 @@ public class JavaApplication16 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola");
-        int i = 25;
+        int i = 30;
     }
     
 }
